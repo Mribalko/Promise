b445cd30cf66bd90e0f13a72baf9807c5371f0d6
Mribalko <mribalko@gmail.com>
2017-05-29 20:20:17 +0300

    Задание 1

---
+++ Part 1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mriba on 29.05.2017.
+ */
+const file = require('./file-promise');
+
+file
+    .read('./data.txt')
+    .then(data => data.toUpperCase())
+    .then(data => file.write('./upper-data.txt', data))
+    .then(filename => console.log(`Создан файл ${filename}`))
+    .catch(err => console.error(err));
--- file-promise.js
+++ file-promise.js
@@ -1,3 +1,29 @@
 /**
  * Created by mriba on 29.05.2017.
  */
+const fs = require('fs');
+const encode = {encoding: 'utf8'};
+
+const read = function (file) {
+    return new Promise(function(resolve, reject){
+        fs.readFile(file, encode, (err, data) => {
+            if(err) reject(err);
+            resolve(data);
+        })
+    })
+};
+
+const write = function (file, data) {
+    return new Promise(function(resolve, reject){
+        fs.writeFile(file, data, (err) => {
+            if(err) reject(err);
+            resolve(file);
+        })
+    })
+};
+
+module.exports = {
+  read,
+  write
+};
+
